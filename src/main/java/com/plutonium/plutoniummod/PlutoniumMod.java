2cd9c0af8cc2f98a740cf14a0419f71c0b402a3e
Luís Barros <luisrochabarros@hotmail.com>
2025-11-01 18:06:18 -0300

    Initial commit - Plutonium Mod

---
+++ src/main/java/com/plutonium/plutoniummod/PlutoniumMod.java
@@ -0,0 +1,28 @@
+package com.plutonium.plutoniummod;
+
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.fml.ModContainer;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.config.ModConfig;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+
+@Mod(PlutoniumMod.MOD_ID)
+public class PlutoniumMod {
+    public static final String MOD_ID = "plutoniummod";
+    public static final Logger LOGGER = LogManager.getLogger();
+
+    public PlutoniumMod() {
+        IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
+
+        modEventBus.addListener(this::commonSetup);
+    }
+
+    private void commonSetup(final FMLCommonSetupEvent event) {
+        LOGGER.info("Setup - PlutoniumMod concluido com sucesso!");
+    }
+}
